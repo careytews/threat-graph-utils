d1be7703a28098638c094444584863c6b00b9ee8
Cyber MacGeddon <cybermaggedon@gmail.com>
2018-05-27 20:28:03 +0100

    Add throttling exception and longer delay

--- apility.py
+++ apility.py
@@ -2,6 +2,18 @@
 import requests
 import json
 
+class RateLimit(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+class ApiError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
 class Apility:
     def __init__(self, uuid):
         self.uuid = uuid
@@ -15,6 +27,13 @@ class Apility:
         headers = { "X-Auth-Token": self.uuid }
 
         r = requests.get(url, headers=headers)
+
+        if r.status_code == 429:
+            raise RateLimit("Rate limit exceeded")
+
+        if r.status_code != 200:
+            raise ApiError(r.text)
+
         return r.json()["response"]
 
     def get_domain_reputation(self, i):
@@ -25,5 +44,12 @@ class Apility:
         headers = { "X-Auth-Token": self.uuid }
 
         r = requests.get(url, headers=headers)
+
+        if r.status_code == 429:
+            raise RateLimit("Rate limit exceeded")
+
+        if r.status_code != 200:
+            raise ApiError(r.text)
+            
         return r.json()["response"]
 
