2dbccfa980bd63e069df421cbb7206c2e34fa089
Cyber MacGeddon <cybermaggedon@gmail.com>
2018-05-29 16:51:32 +0100

    [ch3609] Apility IP update tidied

---
+++ facebook.py
@@ -0,0 +1,62 @@
+
+import requests
+import json
+import urllib
+
+class RateLimit(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+class ApiError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+class Facebook:
+    def __init__(self, id, secret):
+        self.id = id
+        self.secret = secret
+        self.owner_app_id = 820763734618599
+        self.base = "https://graph.facebook.com/"
+
+    def get_ip_report(self, ip):
+        
+        query_params = urllib.urlencode({
+            'access_token': self.id + '|' + self.secret,
+            'type' : "IP_ADDRESS",
+            'owner' : self.owner_app_id,
+            'text' : ip
+        })
+
+        url = "%s/v2.4/threat_descriptors?" % self.base + query_params
+
+        r = requests.get(url)
+        
+        if r.status_code != 200:
+            print "ERROR"
+            raise ApiError(r.text)
+
+        return r.json()
+
+    def get_domain_report(self, domain):
+        
+        query_params = urllib.urlencode({
+            'access_token': self.id + '|' + self.secret,
+            'type' : "DOMAIN",
+            'owner' : self.owner_app_id,
+            'text' : domain
+        })
+
+        url = "%s/v2.4/threat_descriptors?" % self.base + query_params
+
+        r = requests.get(url)
+        
+        if r.status_code != 200:
+            print "ERROR"
+            raise ApiError(r.text)
+
+        return r.json()
+
---
+++ threatgraph.py
@@ -0,0 +1,145 @@
+
+import gaffer
+import sets
+import ipaddress
+import time
+
+class Gaffer(gaffer.Gaffer):
+    def __init__(self):
+        url = "https://analytics.trustnetworks.com/gaffer-threat"
+        gaffer.Gaffer.__init__(self, url)
+        self.use_cert()
+
+    def get_all_ips(self):
+        query = {
+            "class": "uk.gov.gchq.gaffer.operation.OperationChain",
+            "operations": [
+                {
+                    "class": "uk.gov.gchq.gaffer.operation.impl.get.GetAllElements",
+                    "view": {
+                        "entities": {
+                            "ip": {
+                            }
+                        }
+                    }
+                }
+            ]
+        }
+
+        res = self.execute(query)
+        ips = sets.Set([v["vertex"] for v in res])
+
+        return ips
+
+
+    def get_probed_ips(self, probe):
+
+        query = {
+            "class": "uk.gov.gchq.gaffer.operation.OperationChain",
+            "operations": [
+                {
+                    "class": "uk.gov.gchq.gaffer.operation.impl.get.GetAllElements",
+                    "view": {
+                        "entities": {
+                            "ip": {
+                            }
+                        }
+                    }
+                    
+                },
+                {
+                    "class" : "uk.gov.gchq.gaffer.operation.impl.GetWalks",
+                    "operations" : [
+                        {
+                            "class" : "uk.gov.gchq.gaffer.operation.OperationChain",
+                            "operations" : [
+                                {
+                                    "class" : "uk.gov.gchq.gaffer.operation.impl.get.GetElements",
+                                    "includeIncomingOutGoing" : "OUTGOING",
+                                    "view": {
+                                        "edges": {
+                                            "probed": {
+                                                "preAggregationFilterFunctions" : [
+                                                    {
+                                                        "selection" : [ "probe" ],
+                                                        "predicate" : {
+                                                            "class" : "uk.gov.gchq.koryphe.impl.predicate.IsEqual",
+                                                            "value" : probe
+                                                        }
+                                                    }
+                                                ]
+                                            }
+                                        }
+                                    }
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+
+        res = self.execute(query)
+
+        seen_ips = sets.Set([v["entities"][0].keys()[0] for v in res])
+
+        return seen_ips
+
+    def remove_private_ips(self, ips):
+
+        # Remove private addresses
+        private = sets.Set()
+        for v in ips:
+            if ipaddress.ip_address(v).is_private:
+                private.add(v)
+        return ips - private
+
+    def make_match_edge(self, ip, blacklist):
+
+        return {
+ 	    "class": "uk.gov.gchq.gaffer.data.element.Edge",
+ 	    "properties": {
+                "time": {
+                    "uk.gov.gchq.gaffer.time.RBMBackedTimestampSet": {
+                        "timeBucket": "HOUR",
+                        "timestamps": [time.time()]
+                    }
+                }
+            },
+ 	    "group": "matches",
+ 	    "source": ip,
+ 	    "destination": blacklist,
+ 	    "directed": True
+        }
+
+    def make_blacklist_entity(self, blacklist, prob, tp, source, pub):
+
+        return {
+ 	    "class": "uk.gov.gchq.gaffer.data.element.Entity",
+            "properties": {
+                "type": tp,
+                "source": source,
+                "publisher": pub,
+                "update": {
+                    "uk.gov.gchq.gaffer.time.RBMBackedTimestampSet": {
+                        "timeBucket": "HOUR",
+                        "timestamps": [time.time()]
+                        }
+                },
+                "probability": prob
+            },
+            "group": "blacklist",
+            "vertex": blacklist
+        }
+
+    def make_probed_edge(self, thing, prober, probe, time):
+        return {
+ 	    "class": "uk.gov.gchq.gaffer.data.element.Edge",
+            "source": thing,
+            "destination": prober,
+            "properties": {
+                "probe": probe,
+                "probetime": time
+            },
+            "group": "probed"
+        }
